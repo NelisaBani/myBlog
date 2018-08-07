60b48a1a774f40fa79d9f0c283a4711a6be87e76
Nelisa Bani <38911615+NelisaBani@users.noreply.github.com>
2018-08-07 17:17:50 +0200

    Add files via upload
    blog2.0

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,12 @@ router.get('/contact', function(req, res, next) {
 });
 
 // sign-in PAGE
-router.get('/sign-in', function(req, res, next) {
-  res.render('sign-in', { title: 'Sign-in', posts: Posts.posts });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login', posts: Posts.posts });
+});
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('sign_up', { title: 'sign_up', posts: Posts.posts });
 });
 
 // archives page
@@ -28,6 +32,10 @@ router.get('/archives', function(req, res, next) {
 // create page
 router.get('/create', function(req, res, next) {
   res.render('create', { title: 'Create', posts: Posts.posts });
+});
+// edit post
+router.get('/edit', function(req, res, next) {
+  res.render('edit', { title: 'edit', posts: Posts.posts });
 });
 
 // archives page
@@ -56,5 +64,98 @@ function(error, response, body){
 
 });
 });
+
+router.get('/edit/:id', function(req,res,next){
+
+request({
+  url: "http://localhost:8000/posts/" + req.params.id,
+  method: "GET",
+},function(error, response, body) {
+  console.log(JSON.parse(body));
+  // Send  aresponse message
+  res.render('edit', {message: false, posts: JSON.parse(body)});
+});
+
+});
+
+router.post('/edit/:id', function(req, res, next){
+  request({
+    url: "http://localhost:8000/posts/" + req.params.id,
+    method:"PATCH",
+    form:{
+      title: req.body.title,
+      content: req.body.content,
+      authour: req.body.author,
+  }
+}, function(error, response, body) {
+
+
+  res.render('edit',{message:'successfully changed.',posts: JSON.parse(body)});
+});
+
+});
+/* GET subscribe page. */
+router.post('/sign_up', function (req, res, next) {
+
+// gets the last user id and adds 1 to giev a hiugher id
+  // var id = posts.users[posts.users.length-1].id + 1;
+
+// create variable to post
+  var obj = {
+    "id": req.body.id,
+    "user": req.body.user,
+    "email": req.body.email,
+    "password": req.body.password,
+  }
+  //write logic that saves this data
+  request.post({
+    url:'http://localhost:8000/users',
+    body:obj,
+    json:true
+  },
+  function(error,responsive,body){
+    res.redirect('/');
+  });
+
+});
+
+// logging in page
+router.post('/login',function(req,res,next){
+  var users = posts.users;
+  console.log(users);
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  for (let i in users) {
+    console.log(username);
+    if (username == users[i].user && password == users[i].password) {
+        res.render('index', { title: 'Sign up', posts: posts.posts, message: false, logged_in: true});
+    }else{
+        res.render('log_in', { title: 'Sign up', posts: posts.posts, message: "Login unsuccessful, username or password incorrect!"});
+    }
+  }
+});
+/* GET subscribe page. */
+router.post('/subscribe', function (req, res, next) {
+
+// create variable to post
+  var obj = {
+    "name":req.body.name,
+    "email": req.body.email
+  }
+  //write logic that saves this data
+  request.post({
+    url:'http://localhost:8000/subscribe',
+    body:obj,
+    json:true
+  },
+  function(error,responsive,body){
+    res.redirect('/');
+  });
+
+});
+
+
 
 module.exports = router;
